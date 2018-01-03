29cc52f5608f41b506b308b55382106f56b8fe4d
Elijah Nightengale <elijah.charles.smile@gmail.com>
2018-01-03 12:11:27 -0500

    add zoom functionality to list elements

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function initMap(){
     });
 
 function AppViewModel(){
-
+     //hard coding the locations, giving each a lat and lng
      this.locations = ko.observableArray([
         {title: "Espresso Royale", location: {lat: 42.733857, lng: -84.477448}},
         {title: "Strange Matter Coffee", location: {lat: 42.733741, lng: -84.52225}},
@@ -20,17 +20,22 @@ function AppViewModel(){
     ]);
 
     var marker;
-
+    //loop through locations
     for(let i = 0; i < this.locations().length; i++){
-
+      //make a marker for each location on the map
       marker = new google.maps.Marker({
         position: this.locations()[i].location,
         map: map,
         title: this.locations()[i].title,
         animation: google.maps.Animation.DROP
       });
-
     }
-  }
+    //when any of the elements in ul are clicked run this function
+    this.zoomOnLocation = function(){
+      //zooms in on the location you clicked
+      map.setZoom(18);
+      map.setCenter(marker.getPosition());
+    }
+ }
   ko.applyBindings(new AppViewModel());
 }
