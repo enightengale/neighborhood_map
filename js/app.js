9f003e0c7115ba8b1304a2d06a84aae6bb655e3e
Elijah Nightengale <elijah.charles.smile@gmail.com>
2018-01-16 10:09:48 -0500

    make fetch request to yelp reviews

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function initMap(){
 
   var map;
   var lansing = {lat: 42.732536, lng: -84.555534};
-
+  //creating a map by giving it a lat and lng above
   var map = new google.maps.Map(document.getElementById("map"), {
     center: lansing,
     zoom: 11,
@@ -10,6 +10,20 @@ function initMap(){
   });
 
 function AppViewModel(){
+
+  //make a request to yelp reviews
+  fetch("https://api.yelp.com/v3/businesses/gary-danko-san-francisco/reviews", {
+    headers: {
+      Authorization: "Bearer 0W7PDC_nxF9zrWkuPuwLH2yfiCfptMgPhKp9ySfAA5GccWvP7LiCMKG4vHDqMI02OgWi8KngO-DarZrkOxVR6VLbZISD1frYpS-UxWzPU6LnmrYyuZaxLE-p5cVXWnYx"
+    },
+  }).then(response => response.json()).then(getReview)
+
+
+  function getReview(data){
+    const review = data.reviews[0];
+    console.log(review.text);
+  }
+
   //hard coding the locations, giving each a lat and lng
   this.locations = ko.observableArray([
     {title: "Espresso Royale", location: {lat: 42.733857, lng: -84.477448}, img: "https://pbs.twimg.com/profile_images/77346209/866162933_l.jpg"},
@@ -21,8 +35,29 @@ function AppViewModel(){
   ]);
 
   var marker;
+
+
+
   //loop through locations
   for(let i = 0; i < this.locations().length; i++){
+
+
+
+
+    // when any of the elements in ul are clicked run this function
+    this.zoomOnLocation = function(data){
+      //zooms in on the location you clicked
+      map.setZoom(18);
+      map.setCenter(data.location);
+    }
+
+
+
+    //set icon size and url
+    var icon = {
+      scaledSize: new google.maps.Size(50, 50),
+      url: "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/ultra-glossy-silver-buttons-icons-food-beverage/058902-ultra-glossy-silver-button-icon-food-beverage-drink-coffee-tea1.png"
+    }
 
     //make a marker for each location on the map
     marker = new google.maps.Marker({
@@ -30,6 +65,7 @@ function AppViewModel(){
       map: map,
       img: this.locations()[i].img,
       title: this.locations()[i].title,
+      icon: icon,
       animation: google.maps.Animation.DROP
     });
 
@@ -42,19 +78,20 @@ function AppViewModel(){
     //give the markers the infowindow I created above^^
     marker.infowindow = infowindow;
     //when markers clicked open in infowindow
-    marker.addListener("click", function(){
-         this.infowindow.open(map, this);
-    });
-
-    // when any of the elements in ul are clicked run this function
-    this.zoomOnLocation = function(data){
-      //zooms in on the location you clicked
-      map.setZoom(18);
-      map.setCenter(data.location);
+    marker.addListener("click", (function(marker){
+      return function(){
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function() {
+            marker.setAnimation(null);
+          }, 1000);
+      //open infowindow
+      this.infowindow.open(map, this);
     }
+  })(marker));
 
   }
 
  }
+  //apply knockout
   ko.applyBindings(new AppViewModel());
 }
