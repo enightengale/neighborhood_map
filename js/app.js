1bfae7593e56875867caf5944a5ba25a9b17a340
Elijah Nightengale <elijah.charles.smile@gmail.com>
2018-01-17 13:00:19 -0500

    add search functionality

--- js/app.js
+++ js/app.js
@@ -11,6 +11,33 @@ function initMap(){
 
 function AppViewModel(){
 
+
+  //hard coding the locations, giving each a lat and lng
+  this.locations = ko.observableArray([
+    {title: "Espresso Royale", location: {lat: 42.733857, lng: -84.477448}, img: "https://pbs.twimg.com/profile_images/77346209/866162933_l.jpg"},
+    {title: "Strange Matter Coffee", location: {lat: 42.733741, lng: -84.52225}, img: "https://atasteoflansing.files.wordpress.com/2014/08/dsc_0068.jpg"},
+    {title: "Blue Owl Coffee", location: {lat: 42.720327, lng: -84.552019}, img: "http://mediad.publicbroadcasting.net/p/wkar/files/styles/x_large/public/201705/BLUEOWL.JPG"},
+    {title: "Bloom Roasters Coffee", location: {lat: 42.748565, lng: -84.549241}, img: "http://lansingbusinessnews.com/wordpress/wp-content/uploads/2016/08/Bloom1.jpg"},
+    {title: "Iorio's Gelate & Caffé", location: {lat: 42.720089, lng: -84.495629}, img: "https://static1.squarespace.com/static/5188549ce4b0c732a495a649/5642517ee4b0b66656b90c8e/564252d1e4b00b392cc77a7d/1447187166184/12194885_534480993369042_5170890685748847123_o.jpg?format=750w"},
+    {title: "Allegro Coffee Company", location: {lat: 42.728180, lng: -84.452994}, img: "http://assets.wholefoodsmarket.com/www/departments/coffee-tea/AllegroCoffeeStand.jpg"}
+  ]);
+
+  this.places = ko.observableArray(this.locations());
+  this.query = ko.observable("");
+
+  this.search = function(value) {
+    // remove all the current locations, which removes them from the view
+  AppViewModel.this.locations().removeAll();
+
+    for(var x in this.locations()) {
+      if(this.locations()[x].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+        AppViewModel.this.locations().push(this.locations()[x]);
+      }
+    }
+  }
+
+
+
   //make a request to yelp reviews
   fetch("https://api.yelp.com/v3/businesses/gary-danko-san-francisco/reviews", {
     headers: {
@@ -24,20 +51,7 @@ function AppViewModel(){
     console.log(review.text);
   }
 
-  //hard coding the locations, giving each a lat and lng
-  this.locations = ko.observableArray([
-    {title: "Espresso Royale", location: {lat: 42.733857, lng: -84.477448}, img: "https://pbs.twimg.com/profile_images/77346209/866162933_l.jpg"},
-    {title: "Strange Matter Coffee", location: {lat: 42.733741, lng: -84.52225}, img: "https://atasteoflansing.files.wordpress.com/2014/08/dsc_0068.jpg"},
-    {title: "Blue Owl Coffee", location: {lat: 42.720327, lng: -84.552019}, img: "http://mediad.publicbroadcasting.net/p/wkar/files/styles/x_large/public/201705/BLUEOWL.JPG"},
-    {title: "Bloom Roasters Coffee", location: {lat: 42.748565, lng: -84.549241}, img: "http://lansingbusinessnews.com/wordpress/wp-content/uploads/2016/08/Bloom1.jpg"},
-    {title: "Iorio's Gelate & Caffé", location: {lat: 42.720089, lng: -84.495629}, img: "https://static1.squarespace.com/static/5188549ce4b0c732a495a649/5642517ee4b0b66656b90c8e/564252d1e4b00b392cc77a7d/1447187166184/12194885_534480993369042_5170890685748847123_o.jpg?format=750w"},
-    {title: "Allegro Coffee Company", location: {lat: 42.728180, lng: -84.452994}, img: "http://assets.wholefoodsmarket.com/www/departments/coffee-tea/AllegroCoffeeStand.jpg"}
-  ]);
-
   var marker;
-
-
-
   //loop through locations
   for(let i = 0; i < this.locations().length; i++){
 
@@ -91,7 +105,8 @@ function AppViewModel(){
 
   }
 
- }
+}
+  AppViewModel.query.subscribe(AppViewModel.this.search);
   //apply knockout
   ko.applyBindings(new AppViewModel());
 }
